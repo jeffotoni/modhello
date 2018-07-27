450cf0be95a18979ab7e36482cc0577ef5379be1
jefferson <jeff.otoni@gmail.com>
2018-07-27 02:30:21 -0300

    first commit

---
+++ modapp/go.mod
@@ -0,0 +1,3 @@
+module github.com/jeffotoni/modhello/modapp
+
+require github.com/jeffotoni/modhello/modlib v1.0.0
---
+++ modapp/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jeffotoni/modhello/modlib/lib"
+)
+
+func main() {
+
+	run(1, 2)
+}
+
+func run(a, b int) {
+
+	log.Printf("Sum(%d,%d) = %d", a, b, lib.Sum(a, b))
+}
